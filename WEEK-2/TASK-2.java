501dab0943e57874e29386c3cf191823e395ba90
Narender Reddy <vtu28804@veltech.edu.in>
2026-09-14 15:17:36 +0530

    TASK-2
    Implemented two methods: removeElement to remove specified value and removeDuplicates to eliminate duplicates from a sorted array.

---
+++ WEEK-2/TASK-2.java
@@ -0,0 +1,35 @@
+//TASK-2.1
+
+//SAMPLE INPUT AND OUTPUT
+/*Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).*\
+  //CODE
+  class Solution {
+    public int removeElement(int[] nums, int val) {
+       int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+//TASK-2.2(REMOVE DUPLICATES FROM SORTED ARRAY)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
