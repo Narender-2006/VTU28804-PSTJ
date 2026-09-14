9494c72063f22b921545895741b8c584c3a57e06
Narender Reddy <vtu28804@veltech.edu.in>
2026-09-14 15:10:27 +0530

    Find Pivot Index
    Implemented the pivot index algorithm to find the index where the sum of the elements on the left equals the sum of the elements on the right.

---
+++ WEEK-1 /Find Pivot Index.java
@@ -0,0 +1,25 @@
+//SAMPLE INPUT AND OUTPUT
+/*Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11*\
+//CODE
+  class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
